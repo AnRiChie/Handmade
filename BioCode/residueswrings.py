c3f96aa9090498af05c21edb97dcbd6e00b9af9a
AnRiChie <82265267+AnRiChie@users.noreply.github.com>
2024-04-09 01:23:29 +0300

    Improved function
    Changed form of input and output, added new facilities to function.

--- BioCode/residueswrings.py
+++ BioCode/residueswrings.py
@@ -1,37 +1,52 @@
+import sys
 import openbabel as obab
 
 # Create module function that saves set containing residues with input pattern
-def match_res_inf(input_file, pattern):
-    # Sets object for input file
+def match_res_inf(input_file, pattern, rest_inf=True):                  # if rest_inf=False writes only
+    # Sets object for input file                                          indeces of suitable residues
     conv = obab.OBConversion()
-    conv.SetInFormat(conv.FormatFromExt(input_file))    # set up object storing input file
+    conv.SetInFormat(conv.FormatFromExt(input_file))                    # set up object storing input file
 
     # Reads input file and store the SMILES string in variable
     mol = obab.OBMol()
     conv.ReadFile(mol, input_file)
 
-    # Creates set to store residues with rings
-    residues = set()                # not only with rings anymore
+    # Creates list to store information about suitable to pattern residues
+    residues = []
 
     # Iterates through each atom in every peptide's residue, applying SMARTS-pattern to them
     for atom in obab.OBMolAtomIter(mol):
+        res = atom.GetResidue()
+        if (len(residues) != 0) and (residues[-1][0] == res.GetIdx()):  # checks uniqueness of atom (similar to set)
+            continue
         if atom.MatchesSMARTS(pattern):
-            residues.add(atom.GetResidue().GetName())
+            residues.append([res.GetIdx()])                             # writes indeces of residues to list
+            if rest_inf:
+                residues[-1].append(res.GetName())                      # writes the rest of information of
+                chain = res.GetChain()                                  # residue if rest_inf=True
+                if (chain != 'A' and chain != None):
+                    residues[-1].append(chain)
     return residues
 
 # Create private function of root program script
 def main():
     # Creates input pattern for script
-    input_pat = input('input_file SMARTS_pattern output_file\n').split()
+    input_pat = sys.argv[1:]                                            # remade input to set arguments directly
     input_file, pattern = input_pat[0], input_pat[1]
 
     # Now able to find rings with function (if pattern = '[r5,r6]')
     residueswrings = match_res_inf(input_file, pattern)
 
     # Manages our output
+    for i in range(len(residueswrings)): 
+        residueswrings[i][0] = str(residueswrings[i][0])
+        residueswrings[i] = '\t'.join(residueswrings[i])
+    residueswrings = '\n'.join(residueswrings)
+
     if len(input_pat) == 3:
-        with open(input_pat[2], 'w') as out: out.writelines(' '.join(residueswrings))
-    else: print(*residueswrings)
+        with open(input_pat[2], 'w') as out:
+                out.writelines(residueswrings)
+    else: print(residueswrings)
 
 # Code performes only if root program is executed
 if __name__ == '__main__':
